cf23288681878e7e2407e9cb95486564ea275e88
Nicolas <nico.daddabbo7100@gmail.com>
2025-09-11 13:38:58 +0200

    angular: léger refacto error-interceptor switch affichage des erreurs dans le tap

--- front/src/app/shared/interceptors/error-interceptor.ts
+++ front/src/app/shared/interceptors/error-interceptor.ts
@@ -1,47 +1,51 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn, HttpResponse } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import { catchError, throwError } from 'rxjs';
+import { catchError, throwError, tap } from 'rxjs';
 
 export const errorInterceptor: HttpInterceptorFn = (req, next) => {
   const messageService = inject(MessageService);
   const router = inject(Router);
   return next(req).pipe(
+    tap({
+      error: (err: HttpErrorResponse) => {
+        switch (err.status) {
+          case 400:
+            messageService.add({
+              severity: 'error',
+              summary: 'Erreur',
+              detail: 'Requête invalide',
+            });
+            break;
+          case 401:
+            messageService.add({
+              severity: 'error',
+              summary: 'Erreur',
+              detail: 'Non autorisé',
+            });
+            break;
+          case 404:
+            messageService.add({
+              severity: 'error',
+              summary: 'Erreur',
+              detail: 'Non trouvé',
+            });
+            break;
+          default:
+            messageService.add({
+              severity: 'error',
+              summary: 'Erreur',
+              detail: 'Une erreur est survenue',
+            });
+            break;
+        }
+        if (err.status === 401) {
+          router.navigate(['/auth/login']);
+        }
+      },
+    }),
     catchError((err) => {
-      switch (err.status) {
-        case 400:
-          messageService.add({
-            severity: 'error',
-            summary: 'Erreur',
-            detail: 'Requête invalide',
-          });
-          break;
-        case 401:
-          messageService.add({
-            severity: 'error',
-            summary: 'Erreur',
-            detail: 'Non autorisé',
-          });
-          break;
-        case 404:
-          messageService.add({
-            severity: 'error',
-            summary: 'Erreur',
-            detail: 'Non trouvé',
-          });
-          break;
-        default:
-          messageService.add({
-            severity: 'error',
-            summary: 'Erreur',
-            detail: 'Une erreur est survenue',
-          });
-          break;
-      }
-      // if (err.status === 401) {
-      //   router.navigate(['/auth/login']);
-      // }
       return throwError(() => err);
     })
   );
